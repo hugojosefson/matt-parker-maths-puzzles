54f54227ea50269b64d0cd227936fed998590beb
Hugo Josefson <hugo@josefson.org>
2020-10-04 22:21:16 +0200

    Refactor rename to arePrimePairs.

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,12 +18,12 @@ isPrime _ = False
 isPrimePair :: Int -> Int -> Bool
 isPrimePair a b = isPrime $ a + b
 
-areAllPrimePairs :: Int -> Int -> [Int] -> Bool
-areAllPrimePairs a b [] = isPrimePair a b
-areAllPrimePairs a b (c:xs) = isPrimePair a b && areAllPrimePairs b c xs
+arePrimePairs :: Int -> Int -> [Int] -> Bool
+arePrimePairs a b [] = isPrimePair a b
+arePrimePairs a b (c:xs) = isPrimePair a b && arePrimePairs b c xs
 
 isSolution :: [Int] -> Bool
-isSolution (a:b:xs) = areAllPrimePairs a b xs
+isSolution (a:b:xs) = arePrimePairs a b xs
 
 primePairs :: Int -> [[Int]]
 primePairs n = filter isSolution $ permutations [1..n]
