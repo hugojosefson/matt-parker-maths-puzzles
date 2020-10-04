c67d9a2f8b2a918e9cb864273f76f5116fa4596e
Hugo Josefson <hugo@josefson.org>
2020-10-04 20:18:47 +0200

    begin haskell

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+range :: Int -> [Int]
+range max = [1..max]
+
+isPrime :: Int -> Bool
+isPrime 3 = True
+isPrime 5 = True
+isPrime 7 = True
+isPrime 11 = True
+isPrime 13 = True
+isPrime 17 = True
+isPrime 19 = True
+isPrime 23 = True
+isPrime _ = False
+
+isPrimePair :: (Int, Int) -> Bool
+isPrimePair a b = isPrime a+b
+
+--primePairs :: [Int] -> Maybe [Int]
+--primePairs numbers =
