063f3f6cc07e791d47d2f98fa3488f9f964fc371
Hugo Josefson <hugo@josefson.org>
2022-01-03 15:06:14 +0100

    wip: not staged

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
+import Data.List
+import GHC.Float.RealFracMethods
+import Data.Function.Memoize
 
-main :: IO ()
-main = putStrLn someFunc
+isDivisible :: Int -> Int -> Bool
+isDivisible a b = mod a b == 0
+
+intSqrt :: Int -> Int
+intSqrt a = floor $ sqrt $ int2Float a
+
+isPrime :: Int -> Bool
+isPrime n = not $ any (isDivisible n) [2..(intSqrt n)]
+
+isPrimePair :: Int -> Int -> Bool
+isPrimePair a b = isPrime $ a + b
+
+arePrimePairs :: Int -> Int -> [Int] -> Bool
+arePrimePairs a b [] = isPrimePair a b
+arePrimePairs a b (c:xs) = isPrimePair a b && arePrimePairs b c xs
+
+isSolution :: [Int] -> Bool
+isSolution (a:b:xs) = arePrimePairs a b xs
+
+primePairs :: Int -> [[Int]]
+primePairs n = filter isSolution $ permutations [1..n]
+
+primePairsCount :: Int -> Int
+primePairsCount n = length $ primePairs n
